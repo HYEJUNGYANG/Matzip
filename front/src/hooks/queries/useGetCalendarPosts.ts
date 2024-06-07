a549fd52dfb2800b0f3835f39df9f022474ac290
양혜정 <gpwjd248@gmail.com>
2024-06-08 07:20:52 +0900

    캘린더에서 다음 or 이전 달로 이동할 때 깜빡이는 현상 방지

--- front/src/hooks/queries/useGetCalendarPosts.ts
+++ front/src/hooks/queries/useGetCalendarPosts.ts
@@ -1,7 +1,7 @@
 import {ResponseCalendarPost, getCalendarPosts} from '@/api';
 import {queryKeys} from '@/constants';
 import {UseQueryCustomOptions} from '@/types';
-import {useQuery} from '@tanstack/react-query';
+import {keepPreviousData, useQuery} from '@tanstack/react-query';
 
 function useGetCalendarPosts(
   year: number,
@@ -11,8 +11,15 @@ function useGetCalendarPosts(
   return useQuery({
     queryFn: () => getCalendarPosts(year, month),
     queryKey: [queryKeys.POST, queryKeys.GET_CALENDAR_POST, year, month],
+    // 캘린더에서 다음 or 이전 달로 이동할 때(캐시되지 않은 날짜를 가져올 때) 화면에서 목록이 잠깐 사라지는 깜빡임 현상 발생
+    // 데이터를 가져오는 동안 이전 데이터 유지할 수 있는 기능!
+    // 페이지네이션 기능 이용할 때 유용!
+    placeholderData: keepPreviousData,
     ...queryOptions,
   });
 }
+
+// v4
+// keepPreviousData: true
 
 export default useGetCalendarPosts;
